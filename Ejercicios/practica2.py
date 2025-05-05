520e0e4c863533745074da671447bb6108c36aff
Nicolás Diaz <nicolasdiaz13ips@gmail.com>
2025-05-05 19:06:29 -0300

    agrego ej p2

--- Ejercicios/practica2.py
+++ Ejercicios/practica2.py
@@ -55,4 +55,65 @@ def es_conexo(grafo_lista):
     Dado un grafo en representacion de lista, y utilizando la función "componentes_conexas"
     devuelve True/False si el grafo es o no conexo.
     '''
+
+    componentes = componentes_conexas(grafo_lista)
+    return len(componentes) == 1
+
+    pass
+
+def es_completo(grafo_lista):
+    '''
+    Dado un grafo en representacion de lista, devuelve True/False si el grafo es o no completo.
+    Ejemplo Entrada:
+    	['3', 'A', 'B', 'C', 'A B', 'B A', 'A C', 'C A', 'B C', 'C B']
+    Ejemplo formato salida:
+    	True
+    '''
+
+    vertices, aristas = grafo_lista
+    n = len(vertices)
+
+    es_dirigido = all((b,a) in aristas for (a,b) in aristas)
+
+    if es_dirigido:
+        if(len(aristas) != ((n*(n-1))/2)):
+            return False
+
+
+    pass
+    	
+def aristas_de(grafo, vertice):
+    '''
+    Dado un grafo en representacion de lista, devuelva todas las aristas salientes desde un vértice dado
+    Ejemplo Entrada:
+    	grafo = ['3', 'A', 'B', 'C', 'A B', 'A C', 'B C']
+	aristas_de(grafo, 'A')
+    Ejemplo formato salida:
+    	[('A', 'B'), ('A', 'C')]
+    '''
+
+    
+
+    pass
+
+def grafo_inducido(grafo, subconjunto_vertices):
+    '''
+    Dado un grafo en representacion de lista, y un subconjunto de vertices,
+    devuelva el subgrafo inducido
+    Ejemplo Entrada:
+    	grafo = ['4', 'A', 'B', 'C', 'D', 'A B', 'A C', 'B D']
+	subconjunto_vertices = ['A', 'B', 'C']
+    Ejemplo formato salida:
+    	(['A', 'B', 'C'], [('A', 'B'), ('A', 'C')])
+    '''
+    pass
+
+def grafo_complementario(grafo):
+    '''
+    Dado un grafo en representacion de lista, devuelve el grafo complementario en forma de lista
+    Ejemplo Entrada:
+    	['3', 'A', 'B', 'C', 'A B', 'B C']
+    Ejemplo formato salida:
+    	(['A', 'B', 'C'], [('A', 'C'), ('B', 'A'), ('C', 'A'), ('C', 'B')])
+    '''
     pass
